7fa426096a813aa7563474b4740b090430ab2167
462441355@qq.com <462441355@qq.com>
2023-04-18 13:52:06 +0800

    * 优化

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -17,31 +17,6 @@ FilesList.forEach(file => {
         name: basename
       }
     };
-    // 主要 Util 和 Fetch
-    if(basename === 'cdnFetch'){
-      fileNode.output.globals = {
-        'axios': 'axios'
-      };
-    }
-    if(basename === 'cdnPrototype'){
-      fileNode.output.globals = {
-        'js-cookie': 'Cookies',
-        'axios': 'axios',
-        'oment': 'Moment',
-        'lodash': 'Lodash'
-      };
-    }
-    // if(basename === 'cdnTrack'){
-    //   fileNode.output.globals = {
-    //     'axios': 'axios'
-    //   };
-    // }
-    // if(basename === 'cmuTrack'){
-    //   fileNode.output.globals = {
-    //     'axios': 'axios'
-    //   };
-    // }
-
     exportsList.push(fileNode);
 });
 
--- dist/test.js
+++ dist/test.js
@@ -1,2 +1,2 @@
-// (c) 2023 Lin 2023/4/18 下午1:45:03
+// (c) 2023 Lin 2023/4/18 下午1:51:20
 !function(e,t){"object"==typeof exports&&"undefined"!=typeof module?module.exports=t():"function"==typeof define&&define.amd?define(t):(e=e||self).test=t()}(this,function(){"use strict";return{test:function(){console.log(1)}}});
